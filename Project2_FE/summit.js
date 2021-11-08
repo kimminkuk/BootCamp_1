f1e0dd38e26bff1c9013cdc6794841f68c2a85b6
YIKC-16008 <rlaal567@gmail.com>
2021-11-08 17:15:40 +0900

    main branch Update to Project2_FE

---
+++ Project2_FE/summit.js
@@ -0,0 +1,7 @@
+var rad = document.PriorityForm.Prioritybtn;
+var fContainer = document.getElementsByClassName('centerfix_font');
+var prev = null;
+var len = rad.length;
+for (var i = 0; i < len; i++) {
+    rad[i].addE
+}
